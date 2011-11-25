82d593e4c4bc0125e3610f511ad0ec547b452bc6
Alexandru Stan <alex@hypertriangle.com>
2011-11-25 18:41:06 -0500

    used rand for proper placement of head at the start

--- main.c
+++ main.c
@@ -11,40 +11,40 @@
 #define LED 6
 
 #define PERIOD TACCR0
-#define	C3	60300
-#define	Cs3	56914.9505736345
-#define	D3	53720.922154873
-#define	Ds3	50706.1133967601
-#define	E3	47860.2208603847
-#define	F3	45174.0621957505
-#define	Fs3	42636.9891891892
-#define	G3	40244.0969387755
-#define	Gs3	37986.2412713701
-#define	A3	35853.8318181818
-#define	As3	33841.7839368457
-#define	B3	31942.3463189439
-#define	C4	30148.8476092191
-#define	Cs4	28457.4752868172
-#define	D4	26860.4610774365
-#define	Ds4	25352.2418281747
-#define	E4	23929.3844613658
-#define	F4	22586.384331243
-#define	Fs4	21319.0707856969
-#define	G4	20122.0484693878
-#define	Gs4	18993.120635685
-#define	A4	17926.9159090909
-#define	As4	16920.8919684229
-#define	B4	15971.1731594719
-#define	C5	15074.7118967989
-#define	Cs5	14228.4809784079
-#define	D5	13430.0018728824
-#define	Ds5	12676.3246283648
-#define	E5	11964.6922306829
-#define	F5	11293.1921656215
+#define        C3      60300
+#define        Cs3     56914.9505736345
+#define        D3      53720.922154873
+#define        Ds3     50706.1133967601
+#define        E3      47860.2208603847
+#define        F3      45174.0621957505
+#define        Fs3     42636.9891891892
+#define        G3      40244.0969387755
+#define        Gs3     37986.2412713701
+#define        A3      35853.8318181818
+#define        As3     33841.7839368457
+#define        B3      31942.3463189439
+#define        C4      30148.8476092191
+#define        Cs4     28457.4752868172
+#define        D4      26860.4610774365
+#define        Ds4     25352.2418281747
+#define        E4      23929.3844613658
+#define        F4      22586.384331243
+#define        Fs4     21319.0707856969
+#define        G4      20122.0484693878
+#define        Gs4     18993.120635685
+#define        A4      17926.9159090909
+#define        As4     16920.8919684229
+#define        B4      15971.1731594719
+#define        C5      15074.7118967989
+#define        Cs5     14228.4809784079
+#define        D5      13430.0018728824
+#define        Ds5     12676.3246283648
+#define        E5      11964.6922306829
+#define        F5      11293.1921656215
 
 unsigned int song[] = {
-	E4, E4, F4, G4, G4, F4, E4, D4, C4, C4, D4, E4, E4, D4, D4,
-	E4, E4, F4, G4, G4, F4, E4, D4, C4, C4, D4, E4, D4, C4, C4,
+       E4, E4, F4, G4, G4, F4, E4, D4, C4, C4, D4, E4, E4, D4, D4,
+       E4, E4, F4, G4, G4, F4, E4, D4, C4, C4, D4, E4, D4, C4, C4,
 };
 unsigned char nsong = sizeof(song)/sizeof(int);
 
@@ -65,9 +65,11 @@ void io_init(void) {
 	P1OUT=0b00000000;
 }
 
-void timer_init(void) {
+void timer_init(unsigned int on) {
+	PERIOD=0xFFFF;
+	
 	//internal osc, 1xprescaler, up mode
-	TACTL = TASSEL_2 + ID_1 + MC_1;
+	TACTL = TASSEL_2 + ID_1 + MC_1*(on!=0);
 
 	//Enable Interrupts
 	CCTL0 |= CCIE; //Timer
@@ -75,7 +77,7 @@ void timer_init(void) {
 }
 
 void floppy_init(unsigned int pos) {
-	TACTL=0;
+	timer_init(0);
 	
 	unsigned char i;
 	
@@ -105,15 +107,17 @@ int main(void)
 	chip_init();
 	io_init();
 	
-	PERIOD=0xFFFF;
-	timer_init();
+	//Move the cursor to a random place based on input from button
+	set_bit(P1OUT,LED);
 	while(test_bit(P1IN,SW));
-	
-	floppy_init(TAR%100+20);
+	timer_init(1);
+	clear_bit(P1OUT,LED);
+	while(!test_bit(P1IN,SW));
+	srand(TAR);
+	floppy_init(rand()%60+20);
 	set_bit(P1OUT,LED);
 	
-	PERIOD=C3;
-	timer_init();
+	timer_init(1);
 	
 	unsigned char i;
 	while(1) {
